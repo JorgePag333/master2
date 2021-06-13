690c16ebf687162a8fcb5f5708f4cb27cb2dd413
wolfmarcos <estacreado1@gmail.com>
2021-06-13 18:29:34 -0300

    v02 marcos

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="facebook">
+            <img src="assets/icons/social-facebook.png" >
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=2346684247" target="blank" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Whatsapp">
+            <img src="assets/icons/wapp.png" alt="whatsapp-icon">
+        </a>
+        <!--<a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Linkedin">
+            <img src="assets/icons/social-linkedin.png" alt="linkedin-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Pinterest">
+            <img src="assets/icons/social-pinterest.png" alt="pinterest-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/icons/social-twitter.png" alt="twitter-icon">
+        </a>-->
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">El Porteño &copy; 2021</h5>
+</footer>
